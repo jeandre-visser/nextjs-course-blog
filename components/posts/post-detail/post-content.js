15e1b3c1db64a45f38993527b1ed6937b40f2e9f
Jeandre Visser <jeandre@nutrimeals.ca>
2023-04-15 15:41:42 -0600

    Use react markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 
 const DUMMY_POST = {
   slug: 'getting-started-with-nextjs',
   title: 'Getting Started With NextJS',
   image: 'getting-started-nextjs.png',
   date: '2022-02-10',
-  content: '# This is a first post',
+  content: '### This is a first post',
 };
 
 const PostContent = () => {
@@ -16,7 +17,7 @@ const PostContent = () => {
   return (
     <article className={classes.content}>
       <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      {DUMMY_POST.content}
+      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
     </article>
   );
 };

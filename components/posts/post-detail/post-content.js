ec55d1c4d6c506ec3bc06bd1f02c5ea88dc21a0a
Jeandre Visser <jeandre@nutrimeals.ca>
2023-04-15 15:38:11 -0600

    Dummy post for posts detail page

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import PostHeader from './post-header';
+import classes from './post-content.module.css';
+
+const DUMMY_POST = {
+  slug: 'getting-started-with-nextjs',
+  title: 'Getting Started With NextJS',
+  image: 'getting-started-nextjs.png',
+  date: '2022-02-10',
+  content: '# This is a first post',
+};
 
 const PostContent = () => {
-  return <div>PostContent</div>;
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+
+  return (
+    <article className={classes.content}>
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      {DUMMY_POST.content}
+    </article>
+  );
 };
 
 export default PostContent;
--- components/posts/post-detail/post-header.js
+++ components/posts/post-detail/post-header.js
@@ -1,7 +1,21 @@
+import Image from 'next/image';
 import React from 'react';
+import classes from './post-header.module.css';
 
-const PostHeader = () => {
-  return <div>PostHeader</div>;
+const PostHeader = (props) => {
+  const { title, image, date } = props;
+
+  return (
+    <header className={classes.header}>
+      <h1>{title}</h1>
+      <div className={classes.image}>
+        <Image src={image} alt={title} width={200} height={150} />
+      </div>
+      <div className={classes.meta}>
+        <time>{date}</time>
+      </div>
+    </header>
+  );
 };
 
 export default PostHeader;

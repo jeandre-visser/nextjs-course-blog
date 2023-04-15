3810ba5701d187f5822a51bcc56176a5b1163940
Jeandre Visser <jeandre@nutrimeals.ca>
2023-04-15 15:17:52 -0600

    Dummy posts

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,30 @@ import Hero from '@/components/home-page/hero';
 import { Fragment } from 'react';
 import FeaturedPosts from '@/components/home-page/featured-posts';
 
+const DUMMY_POSTS = [
+  {
+    slug: 'getting-started-with-nextjs',
+    title: 'Getting Started With NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'NextJS is the React Framework for Production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR and SSG functionality!',
+    date: '2022-02-10',
+  },
+  {
+    slug: 'getting-started-with-nextjs2',
+    title: 'Getting Started With NextJS2',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'NextJS is the React Framework for Production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR and SSG functionality!',
+    date: '2022-02-10',
+  },
+];
+
 function HomePage() {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   );
 }
